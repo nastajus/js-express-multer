c570d005c36a35834bcd0b45838de22748a6a4d3
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-28 11:31:25 -0400

    unresolved variable file, related to multer middleware

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let port = process.env.PORT || 3004;
-let knex = require('./db/knex');
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+//const bodyParser = require('body-parser');
+const port = process.env.PORT || 3004;
 
-let app = express();
+const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+const storage = multer.diskStorage({
+	destination: './public/uploads/',
+	filename: function(req, file, cb){
+		cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
+	}
+});
 
-app.get('/expenses', function(req, res) {
-	knex.raw('select * from expenses').then(function(expenses) {
-		raw.send(expenses);
-		//element is not imported
-		//referenceerror: raw is not defined
-	})
+const upload = multer({ storage: storage }).any();//single('myImage');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+//below are attempts to somehow "bind middleware" as I understand it, no difference
+//app.use(upload);
+//app.use(multer({dest:'./public/uploads/'}).single('file'));
+
+
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/upload', (req, res) => {
+	upload(req, res, (err) => {
+		if(err){
+			res.render('index', {msg: err});
+		}
+		else {
+			console.log(req.file);
+			//              ^^^^  error here
+			//Webstorm IDE: "unresolved variable file"
+			//Browser upon submit: "Error: Unexpected field" and callstack.
+			//runs but crashes upon submit.
+			//suspect something wrong with typescript, yet included @types/multer & @types/express in package.json.
+			res.send('test');
+		}
+	});
 });
 
 app.listen(port, function () {
